9dbb4a2d36a1e90e48f6a17e46cafb799dd41695
kjs <kjs74962880@gmail.com>
2025-12-22 11:24:49 +0900

    first commit

---
+++ src/main/java/kr/soft/shopping/api/StudyController.java
@@ -0,0 +1,18 @@
+package kr.soft.shopping.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class StudyController {
+
+    @GetMapping("/test1")
+    public String test1() {
+
+        log.info("TEST ACTION");
+
+        return "Hello World";
+    }
+}
